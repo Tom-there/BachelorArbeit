e0827f915adda649e5e4b8fc86ccda7ac788c685
Tom There <tom.schreiner@gmx.de>
2025-01-14 11:02:58 +0100

    cleaned up gui class to be more robust

--- app/src/main/java/de/hhu/cs/stups/algvis/gui/Gui.java
+++ app/src/main/java/de/hhu/cs/stups/algvis/gui/Gui.java
@@ -1,58 +1,20 @@
 package de.hhu.cs.stups.algvis.gui;
 
-import de.hhu.cs.stups.algvis.PluginManager;
-import de.hhu.cs.stups.algvis.gui.actions.ShowPluginAction;
+import de.hhu.cs.stups.algvis.data.structures.Content;
 import de.hhu.cs.stups.algvis.plugins.Plugin;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.HashMap;
+import java.util.Collection;
 
 public class Gui{
-  private static final String WINDOW_TITLE = "AlgVis";
-
   private final JFrame frame;
-  private final JMenuBar menuBar;
-  private final JMenu pluginMenu;
-  private final HashMap<JMenuItem, Plugin> pluginMap;
+  private final JPanel contentPanel;
   private final JToolBar toolBar;
-
-  private JMenuItem test;
-  public Gui(PluginManager pluginManager) {
+  public Gui(Collection<Plugin> installedPlugins) {
     // INIT Frame
     // CONTENTS: MenuBar - ContentPanel
-    frame = new JFrame(WINDOW_TITLE);
-    initJFrame();
-
-    // ADD ContentPanel
-    frame.add(pluginManager.getContentPanel(), BorderLayout.CENTER);
-
-    // INIT MenuBar
-    // CONTENTS: Plugins dropdown
-    menuBar = new JMenuBar();
-    menuBar.setBackground(Color.red);
-    frame.add(menuBar, BorderLayout.NORTH);
-
-    // INIT Plugins dropdown
-    pluginMenu = new JMenu("Plugins");
-    menuBar.add(pluginMenu);
-    pluginMap = new HashMap<>();
-    for (Plugin plugin : pluginManager.getInstalledPlugins()) {
-      JMenuItem menuItem = new JMenuItem(plugin.getName());
-      menuItem.addActionListener(new ShowPluginAction(plugin, pluginManager, frame));
-      pluginMenu.add(menuItem);
-      pluginMap.put(menuItem, plugin);
-    }
-
-    // INIT toolbar
-    // CONTENTS: next
-    toolBar = pluginManager.getToolbar();
-    frame.add(toolBar, BorderLayout.SOUTH);
-    frame.setVisible(true);
-  }
-
-  private void initJFrame() {
-    //set sizes
+    frame = new JFrame("AlgVis");
     frame.setMinimumSize(new Dimension(720, 480));
     frame.setPreferredSize(new Dimension(1600, 900));
     frame.setMaximumSize(new Dimension(1920, 1080));
@@ -60,5 +22,67 @@ public class Gui{
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
+
+    // INIT ContentPanel
+    contentPanel = new JPanel();
+    contentPanel.setMinimumSize(new Dimension(720, 480));
+    contentPanel.setPreferredSize(new Dimension(1600, 900));
+    contentPanel.setMaximumSize(new Dimension(1920, 1080));
+    contentPanel.setLayout(new BorderLayout());
+    contentPanel.setBackground(Color.DARK_GRAY);
+    contentPanel.add(genSplashScreen(), BorderLayout.CENTER);
+    frame.add(contentPanel, BorderLayout.CENTER);
+
+    // INIT MenuBar
+    JMenuBar menuBar = new JMenuBar();
+    frame.add(menuBar, BorderLayout.NORTH);
+
+    // INIT Plugins dropdown
+    JMenu pluginMenu = new JMenu("Plugins");
+    menuBar.add(pluginMenu);
+    for (Plugin plugin : installedPlugins) {
+      JMenuItem menuItem = new JMenuItem(plugin.getName());
+      menuItem.addActionListener(e -> this.displayPlugin(plugin));
+      pluginMenu.add(menuItem);
+    }
+
+    // INIT toolbar
+    toolBar = new JToolBar();
+    frame.add(toolBar, BorderLayout.SOUTH);
+
+    refreshFrame();
+  }
+  public void refreshFrame(){frame.setVisible(true);}
+  public void displayPlugin(Plugin plugin){
+    switchToPlugin(plugin);
+    setToolbarButtons(plugin.getToolBarButtons());
+    plugin.onPluginLoad();
+  }
+  private void switchToPlugin(Plugin plugin){
+    contentPanel.removeAll();
+    for (Content content : plugin.getGuiElements()) {
+      switch (content.getLocation()){
+        case Content.Location.left -> contentPanel.add(content.getSwingComponent(), BorderLayout.WEST);
+        case Content.Location.right -> contentPanel.add(content.getSwingComponent(), BorderLayout.EAST);
+        case Content.Location.center -> contentPanel.add(content.getSwingComponent(), BorderLayout.CENTER);
+      }
+    }
+    contentPanel.setVisible(true);
+    refreshFrame();
+  }
+  private void setToolbarButtons(Collection<ToolBarButton> enabledButtons){
+    toolBar.removeAll();
+    for (ToolBarButton button: enabledButtons){
+      JButton jButton = new JButton(button.getText());
+      jButton.addActionListener(e -> button.action());
+      toolBar.add(jButton);
+    }
+  }
+  private static JPanel genSplashScreen(){
+    JPanel splashScreen = new JPanel();
+    splashScreen.setBackground(Color.lightGray);
+    JLabel text = new JLabel("welcome");
+    splashScreen.add(text);
+    return splashScreen;
   }
 }
